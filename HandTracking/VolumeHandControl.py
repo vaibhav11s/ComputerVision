bfbcfec5dcd3a37b9aba14aa4bf59a6452a7bddc
Vaibhav <vaibhavsathawane11@gmail.com>
2021-09-12 00:18:12 +0530

    HandTracking

---
+++ HandTracking/VolumeHandControl.py
@@ -0,0 +1,60 @@
+import cv2
+import time
+import numpy as np
+import HandTrakingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam , hCam = 640,480
+cap = cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(3,hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.7)
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+
+while True:
+    success, img = cap.read()
+    detector.findHands(img)
+    lmList = detector.findPosition(img,draw=False)
+    if len(lmList) != 0:
+        x1,y1 = lmList[4][1],lmList[4][2]
+        x2,y2 = lmList[8][1],lmList[8][2]
+        cx,cy = (x1+x2)//2 , (y1+y2) // 2
+        cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
+        cv2.circle(img,(x2,y2),15,(255,0,255),cv2.FILLED)
+        cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+        length = math.hypot(x2-x1,y2-y1)
+        # handRange 50-250
+        # VolumeRange -65.25,0
+        vol = np.interp(length,[50,250],[minVol,maxVol])
+        volume.SetMasterVolumeLevel(vol,None)
+        print(vol)
+        if length < 50:
+            cv2.circle(img,(cx,cy),15,(0,255,0),cv2.FILLED)
+
+
+
+    # show FPS
+    # cTime = time.time()
+    # fps = 1/(cTime-pTime)
+    # pTime = cTime
+    # cv2.putText(img,str(int(fps)),(40,70),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0))
+
+    cv2.imshow("Img",img)
+    cv2.waitKey(1)
